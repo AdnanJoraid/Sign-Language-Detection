4df570d0a0c83a353c17e9d18084a0fe78ab44c4
adnanjoraid <adnanjoraidca@gmail.com>
2021-08-30 12:38:30 -0400

    Initial Commit

---
+++ ASLRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ASLRecognition
+//
+//  Created by Adnan Joraid on 2021-08-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
